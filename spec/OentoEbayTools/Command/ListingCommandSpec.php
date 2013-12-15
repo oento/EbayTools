8f8bdf5fd65363b600a16e191d9838cb3b40bcdb
Adrian Cendrowski <leettastic@gmail.com>
2013-12-15 10:11:21 +0000

    Removed unused use statements

--- spec/OentoEbayTools/Command/ListingCommandSpec.php
+++ spec/OentoEbayTools/Command/ListingCommandSpec.php
@@ -1,12 +1,6 @@
 <?php
 namespace spec\OentoEbayTools\Command;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-
 use PhpSpec\ObjectBehavior;
 
 class ListingCommandSpec extends ObjectBehavior
